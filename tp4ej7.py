4430d729c75b04aa51ca8b70ac2e9154b19cd11d
SantiiV <santitato.12@gmail.com>
2021-06-07 16:31:39 -0300

    EJ 7
    Retorno los resultados y los imprimo en prueba.
    Divido la línea en dos (¿de esta manera?)
    También levanto una excepción por si se intenta dividir por 0

--- tp4ej7.py
+++ tp4ej7.py
@@ -3,20 +3,26 @@
 # UNRN Andina - Introducción a la Ingeniería en Computación
 ################
 
+class IngresoIncorrecto(Exception):
+    pass
+
 def division_lenta(dividendo, divisor):
     cociente = 0
     while dividendo >= divisor: 
         dividendo = dividendo - divisor
-        cociente += 1
-        
-    print("El cociente es:", cociente)
-    print("El resto es:", dividendo)
+        cociente += 1  
+    return cociente, dividendo
              
    
 def prueba():
     dividendo = int(input("Ingrese el dividendo "))
     divisor = int(input("Ingrese el divisor "))
-    print(division_lenta(dividendo, divisor))
+    if divisor == 0:
+        raise IngresoIncorrecto("Error. El divisor no debe ser 0.")
+        
+    print("El cociente y resto son:")
+    valores = division_lenta(dividendo, divisor)
+    print(valores)
 
 
 if __name__ == "__main__":
